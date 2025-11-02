58c6313826c529c2883927567ffa3020dcd9de4b
James Dunlop <jamezdunlop@gmail.com>
2025-11-03 11:38:14 +1300

    changing the nav component

---
+++ 46WindowTinting/windowtinting/src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "../nav/nav";
+
+function Header() {
+    return (
+        <header className="main-header">
+          <Nav />
+        </header>
+    );
+}
